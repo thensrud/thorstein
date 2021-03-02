8053d4100535db335926495626957d9604b66b25
thensrud <thensrud@hotmail.com>
2021-03-02 16:32:16 +0100

    Fixed town

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -80,15 +80,15 @@ export const Home = () => {
               </a>
             </h2>
             
-            <img src={manIcon} className="icon"></img>
             <p className="text-box-paragraph">
-              I made this site for fun, and as a place to collect and showcase some of my practice projects. Testing.
+              I made this site for fun, and as a place to collect and showcase some of my practice projects.
               <br/><br/>
-              Above, you can see my home town, Rjukan, viewed more or less from the perspective of the house in which I grew up, made with CSS and unhealthy amounts of time.
+              Above, you can see my home town, Rjukan, viewed more or less from the perspective of the house in which I grew up, made with CSS.
               You can read more about what I do and what I've done by clicking on the button below.
               <br/><br/>
               Coming soon.
             </p>
+            <img src={manIcon} className="icon"></img>
             <div className="button-holder">
               <button className="button gradient-border">
                 About me
@@ -99,48 +99,48 @@ export const Home = () => {
           <div className="text-box">
             <h1 className="title">Games</h1>
             <h2 className="sub-title">Made in JavaScript.</h2>
-            <img src={gameConsoleIcon} className="icon"></img>
             <p className="text-box-paragraph">
               Coming soon.
             </p>
+            <img src={gameConsoleIcon} className="icon"></img>
             <div className="button-holder">
-              <button className="button gradient-border">Games</button>
+              <button className="button gradient-border">Play</button>
             </div>
           </div>
 
           <div className="text-box">
             <h1 className="title">Music</h1>
             <h2 className="sub-title">Some good stuff.</h2>
-            <img src={musicIcon} className="icon"></img>
             <p className="text-box-paragraph">
               Coming soon.
             </p>
+            <img src={musicIcon} className="icon"></img>
             <div className="button-holder">
-              <button className="button gradient-border">Music</button>
+              <button className="button gradient-border">Listen</button>
             </div>
           </div>
 
           <div className="text-box">
             <h1 className="title">Cool things</h1>
             <h2 className="sub-title">Using APIs and whatnot.</h2>
-            <img src={thingsIcon} className="icon"></img>
             <p className="text-box-paragraph">
               Coming soon. Calm down.
             </p>
+            <img src={thingsIcon} className="icon"></img>
             <div className="button-holder">
-              <button className="button gradient-border">COOL THINGS</button>
+              <button className="button gradient-border">Try</button>
             </div>
           </div>
 
           <div className="text-box">
-            <h1 className="title">Books and articles</h1>
+            <h1 className="title">Books & articles</h1>
             <h2 className="sub-title">Interesting stuff.</h2>
-            <img src={readIcon} className="icon"></img>
             <p className="text-box-paragraph">
               Coming soon. Jeeez.
             </p>
+            <img src={readIcon} className="icon"></img>
             <div className="button-holder">
-              <button className="button gradient-border">Blog</button>
+              <button className="button gradient-border">Read</button>
             </div>
           </div>
 
